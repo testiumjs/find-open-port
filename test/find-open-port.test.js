2a6dfa2a9b895e703ad988a30a1003b8c6859f88
Jan Krems <jan.krems@groupon.com>
2016-03-23 16:07:31 -0700

    refactor: Follow common project structure

--- test/find-open-port.test.js
+++ test/find-open-port.test.js
@@ -1,10 +1,47 @@
 'use strict';
+var net = require('net');
+
 var assert = require('assertive');
 
-var findOpenPort = require('../');
+var findPort = require('../');
 
-describe('find-open-port', function () {
-  it('is empty', function () {
-    assert.deepEqual({}, findOpenPort);
+describe('Finding an open port', function () {
+  it('can listen on the returned port', function (done) {
+    findPort().then(function (port) {
+      var server = net.createServer();
+      server.listen(port, function () {
+        assert.equal(port, server.address().port);
+        server.close();
+        done();
+      });
+    });
+  });
+});
+
+describe('Checking if a port is available', function () {
+  var port;
+  before(function () {
+    return findPort().then(function (p) { port = p; });
+  });
+
+  it('returns an available port', function () {
+    return findPort.isAvailable(port)
+      .then(function (available) {
+        assert.expect(available);
+      });
+  });
+
+  describe('after listening on the port', function () {
+    before(function (done) {
+      var server = net.createServer();
+      server.listen(port, done);
+    });
+
+    it('is no longer available', function () {
+      return findPort.isAvailable(port)
+        .then(function (available) {
+          assert.expect(!available);
+        });
+    });
   });
 });
